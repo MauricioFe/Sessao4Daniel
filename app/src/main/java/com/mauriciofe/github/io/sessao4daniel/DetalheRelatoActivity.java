85020e3e1f5f170e3949475b41bb162910333da7
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-02-21 15:44:43 -0300

    Configurando menu de acordo com a orientação da tela

--- app/src/main/java/com/mauriciofe/github/io/sessao4daniel/DetalheRelatoActivity.java
+++ app/src/main/java/com/mauriciofe/github/io/sessao4daniel/DetalheRelatoActivity.java
@@ -1,8 +1,14 @@
 package com.mauriciofe.github.io.sessao4daniel;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -39,5 +45,27 @@ public class DetalheRelatoActivity extends AppCompatActivity {
         txtNomeRelator.setText(relato.getNomeUsuario());
         txtTelefone.setText(relato.getTelefone());
         txtEmail.setText(relato.getEmail());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_detalhe_relato, menu);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            MenuItem item = menu.findItem(R.id.action_edit);
+            MenuItem item2 = menu.findItem(R.id.action_delete);
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+            item2.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_edit) {
+            Toast.makeText(this, "editando", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.action_delete)
+            Toast.makeText(this, "Excluind", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
